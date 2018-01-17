c8590b0c572bef630e3e2b0ec808f63eb49363dc
Curt Bonade <curt.bonade@gmail.com>
2018-01-17 16:23:21 -0500

    add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose.js");
 var { Todo } = require("./models/todo.js");
@@ -32,6 +33,35 @@ app.get("/todos", (req, res) => {
       res.status(400).send(e);
     }
   );
+});
+
+//GET /todos/1234324
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+
+  //Validate id using isValid
+  if (!ObjectID.isValid(id)) {
+    //stop execution, 404 - send back empty send
+    return res.status(404).send();
+  }
+  //findById
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+
+    .catch(e => {
+      res.status(400).send();
+    });
+  //success
+  //if todo - send it back
+  //if no todo - send back 404 with empty body
+  //error
+  //400 - and send empty body back
 });
 
 app.listen(3000, () => {
